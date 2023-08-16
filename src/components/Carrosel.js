d23c0b8d18e49bd2451e97e086a1fc4bdb8dc686
Cassio-Ares <cassiotadeu.ads@gmail.com>
2023-08-15 22:33:45 -0300

    Lp Carrossel JS

---
+++ src/components/Carrosel.js
@@ -0,0 +1,46 @@
+import styles from "./Carrossel.module.css";
+import cardBru from "../image/cardBru.svg";
+import cardLeo from "../image/cardLeo.svg";
+import cardSama from "../image/cardSama.svg";
+import setaDire from '../image/setaDire.svg';
+import setaEsq from '../image/setaEsq.svg'
+
+
+function Carrossel() {
+    
+    var setaEsqe = document.getElementById("setaE");
+    var cardBrun= document.getElementById('cardBru');
+    var cardLeon = document.getElementById('cardLeo');
+    var cardSaman = document.getElementById('cardSama');
+    var setaDiret = document.getElementById('setaD')
+
+    function clickEsquerda(){
+        setaEsqe.style = "display:none"
+        cardBrun.style = "display:none"
+        cardSaman.style = "display:flex"         
+    }
+
+    function clickDireita(){
+        setaEsqe.style = "display:flex"
+        cardBrun.style = "display:flex"
+        cardSaman.style = "display:none"       
+    }
+    
+    
+
+  return (
+    <div className={styles.carrossel}>
+      <h2>Mais de 500 alunos já iniciaram seus estudos</h2>
+      <section>
+        <img  id="setaE" onClick={clickEsquerda} src={setaEsq} />
+        <img id="cardBru" src={cardBru} />
+        <img id="cardLeo" src={cardLeo}/>
+        <img id="cardSama" src={cardSama}/>
+        <img id="setaDire" onClick={clickDireita} src={setaDire} />
+      </section>
+    </div>
+    
+  );
+}
+
+export default Carrossel;
